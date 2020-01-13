732cb3dafe9a719063a309a5dcb213ed9f3ab3f5
penincillin <rongyu9124@hotmail.com>
2020-01-13 12:38:12 +0800

    add new function for ry_utils

--- ry_utils.py
+++ ry_utils.py
@@ -1,35 +1,16 @@
 # file to store some often use functions
-
 import os, sys, shutil
 import os.path as osp
 import multiprocessing as mp
-
-
-def print_vars(_locals, variables, func):
-    '''
-    print variable in the variables,
-    the format is [name_of_variable, value_of_variable]
-    useage:
-    print_vars(locals(), variables)
-    '''
-    print('------------------------------------')
-    var_list = list()
-    for name, variable in list(_locals.items()):
-        if variable in variables:
-            idx = variables.index(variable)
-            var_list.append( (name, variable, idx) )
-
-    sorted_var_list = sorted(var_list, key=lambda a:a[2])
-    for name, variable, _ in sorted_var_list:
-        print("{}\t{}".format(name, func(variable)))
-    print('------------------------------------')
-
+import numpy as np
+import cv2
 
 
 def renew_dir(target_dir):
     if osp.exists(target_dir):
         shutil.rmtree(target_dir)
     os.makedirs(target_dir)
+
 
 def build_dir(target_dir):
     if not osp.exists(target_dir):
@@ -45,14 +26,13 @@ def remove_swp(in_dir):
                 os.remove(full_path)
 
 
-def easy_parallel(target_func, all_args):
-    process_list = list()
-    for args in all_args:
-        p = mp.Process(target=target_func, args=args)
-        p.start()
-        process_list.append(p)
-    for p in process_list:
-        p.join()
+def remove_pyc(in_dir):
+    remove_files = list()
+    for subdir, dirs, files in os.walk(in_dir):
+        for file in files:
+            if file.endswith('.pyc'):
+                full_path = osp.join(subdir,file)
+                os.remove(full_path)
 
 
 def md5sum(file_path):
@@ -61,3 +41,49 @@ def md5sum(file_path):
     with open(file_path, 'rb') as in_f:
         hash_md5.update(in_f.read())
     return hash_md5.hexdigest()
+
+
+def draw_keypoints_each(image, keypoints, res_dir):
+    if isinstance(image, str):
+        image = cv2.imread(image)
+    elif isinstance(image, np.ndarray):
+        image = image
+    else:
+        print("Undefined image type")
+        sys.exit(0)
+    renew_dir(res_dir)
+    for i in range(len(keypoints)):
+        if len(keypoints[i]) > 2:
+            x, y, score = keypoints[i]
+            if score > 0.0:
+                draw_image = image.copy()
+                cv2.circle(draw_image, (int(x), int(y)), radius=5,
+                           color=(0, 0, 255), thickness=-1)
+        else:
+            draw_image = image.copy()
+            x, y = keypoints[i]
+            cv2.circle(draw_image, (int(x), int(y)), radius=5,
+                       color=(0, 0, 255), thickness=-1)
+        cv2.imwrite('{0}/{1:02d}.png'.format(res_dir, i), draw_image)
+
+
+def draw_keypoints(image, keypoints, res_img_path):
+    if isinstance(image, str):
+        image = cv2.imread(image)
+    elif isinstance(image, np.ndarray):
+        image = image
+    else:
+        print("Undefined image type")
+        sys.exit(0)
+    draw_image = image
+    for i in range(len(keypoints)):
+        if len(keypoints[i]) > 2:
+            x, y, score = keypoints[i]
+            if score > 0.0:
+                cv2.circle(draw_image, (int(x), int(y)), radius=5,
+                           color=(0, 0, 255), thickness=-1)
+        else:
+            x, y = keypoints[i]
+            cv2.circle(draw_image, (int(x), int(y)), radius=5,
+                       color=(0, 0, 255), thickness=-1)
+    cv2.imwrite(res_image_path, draw_image)
